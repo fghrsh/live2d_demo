20bcb92a89fe9185bf6df56759f4d118756e21b1
FGHRSH <fghrsh@wxw.moe>
2019-06-13 23:16:28 +0800

    fix #4

--- assets/waifu-tips.js
+++ assets/waifu-tips.js
@@ -261,14 +261,14 @@ function loadTipsMessage(result) {
         var text;
         if (window.location.href == live2d_settings.homePageUrl) {
             var now = (new Date()).getHours();
-            if (now > 23 || now <= 5) text = getRandText(result.waifu.hour_tips.t23-5);
-            else if (now > 5 && now <= 7) text = getRandText(result.waifu.hour_tips.t5-7);
-            else if (now > 7 && now <= 11) text = getRandText(result.waifu.hour_tips.t7-11);
-            else if (now > 11 && now <= 14) text = getRandText(result.waifu.hour_tips.t11-14);
-            else if (now > 14 && now <= 17) text = getRandText(result.waifu.hour_tips.t14-17);
-            else if (now > 17 && now <= 19) text = getRandText(result.waifu.hour_tips.t17-19);
-            else if (now > 19 && now <= 21) text = getRandText(result.waifu.hour_tips.t19-21);
-            else if (now > 21 && now <= 23) text = getRandText(result.waifu.hour_tips.t21-23);
+            if (now > 23 || now <= 5) text = getRandText(result.waifu.hour_tips['t23-5']);
+            else if (now > 5 && now <= 7) text = getRandText(result.waifu.hour_tips['t5-7']);
+            else if (now > 7 && now <= 11) text = getRandText(result.waifu.hour_tips['t7-11']);
+            else if (now > 11 && now <= 14) text = getRandText(result.waifu.hour_tips['t11-14']);
+            else if (now > 14 && now <= 17) text = getRandText(result.waifu.hour_tips['t14-17']);
+            else if (now > 17 && now <= 19) text = getRandText(result.waifu.hour_tips['t17-19']);
+            else if (now > 19 && now <= 21) text = getRandText(result.waifu.hour_tips['t19-21']);
+            else if (now > 21 && now <= 23) text = getRandText(result.waifu.hour_tips['t21-23']);
             else text = getRandText(result.waifu.hour_tips.default);
         } else {
             var referrer_message = result.waifu.referrer_message;
